0a2bf4961e6929f4bb5b17752e2a71fb260d8d33
Spinkll <roma.skorohod.2005@gmail.com>
2023-12-25 22:38:03 +0200

    fix swagger

--- src/modules/shelfs/shelfs.controller.ts
+++ src/modules/shelfs/shelfs.controller.ts
@@ -2,13 +2,18 @@ import { Controller, Get, Query, UseGuards, Request } from '@nestjs/common';
 import { ShelfsService } from './shelfs.service';
 import { FindShelfsRequestDto } from 'src/dto/shelfs';
 import { AccessTokenGuard } from 'src/guards';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Shelfs')
 @Controller('shelfs')
 export class ShelfsController {
   constructor(private readonly shelfsService: ShelfsService) {}
 
+  @ApiOkResponse({
+    description: 'Search Shelfs ',
+    type: FindShelfsRequestDto,
+    isArray: true,
+  })
   @UseGuards(AccessTokenGuard)
   @Get()
   async findShelfs(
