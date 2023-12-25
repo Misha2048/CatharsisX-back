9e21d1102bbdacfd2244f2ab106f11737724c2a8
Spinkll <95828180+Spinkll@users.noreply.github.com>
2023-12-25 14:49:56 +0200

    add search stillages (#13)

--- src/modules/stillages/stillages.service.ts
+++ src/modules/stillages/stillages.service.ts
@@ -1,24 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import client from '../../db/prismaClient';
 import { Stillage } from '@prisma/client';
+import { FindStillagesRequestDto } from 'src/dto/stillages';
 
 @Injectable()
 export class StillagesService {
+  async findStillages(
+    findStillagesRequestDto: FindStillagesRequestDto,
+    userId: string,
+  ): Promise<Stillage[]> {
+    const filter = Object.entries(findStillagesRequestDto).reduce(
+      (filters, [key, value]) => {
+        if (value !== undefined && value !== '') {
+          filters[key] =
+            typeof value === 'string'
+              ? { contains: value }
+              : { gte: new Date(value) };
+        }
+        return filters;
+      },
+      {},
+    );
 
-    async updateStillage(id: string, userId: string, opts: any): Promise<Stillage | null | undefined>{
-        try{
-            return await client.stillage.update({
-                where: {
-                    id,
-                    userId,
-                },
-                data: opts,
-            });
-        }
-        catch(error){
-            return undefined;
-        }
+    return await client.stillage.findMany({
+      where: {
+        user: { id: userId },
+        ...filter,
+      },
+    });
+  }
+
+  async updateStillage(
+    id: string,
+    userId: string,
+    opts: any,
+  ): Promise<Stillage | null | undefined> {
+    try {
+      return await client.stillage.update({
+        where: {
+          id,
+          userId,
+        },
+        data: opts,
+      });
+    } catch (error) {
+      return undefined;
     }
-
-
+  }
 }
