cd6c2eff799111ef868b87e95a528877b1bcdd9a
Spinkll <roma.skorohod.2005@gmail.com>
2024-01-06 16:19:44 +0200

    add upload file

---
+++ src/dto/file.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UploadFileRequest {
+  @ApiProperty()
+  filename: string;
+
+  @ApiProperty()
+  file_size: number;
+
+  @ApiProperty()
+  content: Buffer;
+
+  @ApiProperty()
+  shelf_id: string;
+
+  @ApiProperty()
+  stillage_id: string;
+}
---
+++ src/modules/files/files.service.ts
@@ -0,0 +1,99 @@
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { UploadFileRequest } from 'src/dto/file';
+import client from 'src/db/prismaClient';
+import { catchError, firstValueFrom } from 'rxjs';
+import { AxiosError } from 'axios';
+
+@Injectable()
+export class FilesService {
+  private readonly logger = new Logger(FilesService.name);
+  constructor(private readonly httpService: HttpService) {}
+
+  async validateFile(content: Buffer): Promise<boolean> {
+    try {
+      if (process.env.VALIDATE_FILES === 'true') {
+        const { data } = await firstValueFrom(
+          this.httpService
+            .post(
+              'http://127.0.0.1:8000/validate/file/',
+              { text: content.toString() },
+              {
+                headers: {
+                  'x-api-key': process.env.x_api_key,
+                },
+              },
+            )
+            .pipe(
+              catchError((error: AxiosError) => {
+                if (error.response && error.response.data) {
+                  this.logger.error(
+                    `Validation error: ${JSON.stringify(error.response.data)}`,
+                  );
+                } else {
+                  this.logger.error(`Validation error: ${error.message}`);
+                }
+
+                throw new HttpException(
+                  'An error happened during file validation!',
+                  HttpStatus.INTERNAL_SERVER_ERROR,
+                );
+              }),
+            ),
+        );
+        console.log(data);
+        return data;
+      } else {
+        return true;
+      }
+    } catch (error) {
+      this.logger.error(`Failed to validate file: ${error.message}`);
+      throw new HttpException(
+        'Failed to validate file!',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async uploadFileToShelf(
+    uploadFileRequest: UploadFileRequest,
+    file: Express.Multer.File,
+  ): Promise<void> {
+    try {
+      const isValid = await this.validateFile(file.buffer);
+
+      if (!isValid) {
+        this.logger.error('Validation failed. File not uploaded.');
+        throw new HttpException('Invalid file!', HttpStatus.BAD_REQUEST);
+      } else {
+        await client.file.create({
+          data: {
+            filename: uploadFileRequest.filename,
+            text_content: file.buffer.toString('utf-8'),
+            content: file.buffer,
+            size: file.size,
+            uploaded_at: new Date(),
+            shelf: {
+              connect: {
+                id: uploadFileRequest.shelf_id,
+              },
+            },
+            stillage: {
+              connect: {
+                id: uploadFileRequest.stillage_id,
+              },
+            },
+          },
+        });
+
+        this.logger.log('File uploaded successfully.');
+      }
+    } catch (error) {
+      this.logger.error(`Failed to upload file to shelf: ${error.message}`);
+      throw new HttpException(
+        'Failed to upload file to shelf!',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
