f11536597ae63a9eb6138eeaccf11bc1bf5b7a9f
Spinkll <roma.skorohod.2005@gmail.com>
2024-01-20 18:06:54 +0200

    delete responseDto in catalog

--- src/modules/catalog/catalog.controller.ts
+++ src/modules/catalog/catalog.controller.ts
@@ -1,20 +1,15 @@
 import { Controller, Get, UseGuards, Query, Req } from '@nestjs/common';
 import { CatalogService } from './catalog.service';
-import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AccessTokenGuard } from 'src/guards';
 import { Request } from 'express';
-import { GetCatalogRequestDto, GetCatalogResponseDto } from 'src/dto/catalog';
+import { GetCatalogRequestDto } from 'src/dto/catalog';
 
 @ApiTags('Catalog')
 @Controller('catalog')
 export class CatalogController {
   constructor(private readonly catalogService: CatalogService) {}
 
-  @ApiOkResponse({
-    description: 'Get Catalog ',
-    type: GetCatalogResponseDto,
-    isArray: true,
-  })
   @ApiBearerAuth()
   @UseGuards(AccessTokenGuard)
   @Get()
