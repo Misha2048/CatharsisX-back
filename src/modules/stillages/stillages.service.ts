85d88111c1d83a7f6258d4778b3df10ad253f6e2
Ivan Holotsvan <iholotsvan@gmail.com>
2024-01-23 13:44:33 +0200

    fix that dateFrom was as number

--- src/modules/stillages/stillages.service.ts
+++ src/modules/stillages/stillages.service.ts
@@ -19,12 +19,12 @@ export class StillagesService {
           if (key === 'stillage') {
             filters[key] = { id: value };
           } else if (key === 'last_upload_at' || key === 'created_at') {
-            const dateFrom = new Date(value[0]).setHours(0, 0, 0, 0);
+            const dateFrom = new Date(value[0]);
+            dateFrom.setHours(0, 0, 0, 0);
             const dateTo = new Date(value[1]);
             dateTo.setDate(dateTo.getDate() + 1);
             dateTo.setHours(0, 0, 0, 0);
-
-            filters[key] = { gte: dateFrom, lt: dateTo };
+            filters[key] = { gte: new Date(dateFrom), lt: new Date(dateTo) };
           } else {
             filters[key] = { contains: value };
           }
