fc8c8dbfc47f04c924fff7d64dfe06bd1f6608ce
Spinkll <roma.skorohod.2005@gmail.com>
2024-01-25 20:22:46 +0200

    add reading xlsx,rtf,docx,pdf,pptx

--- src/modules/files/files.service.ts
+++ src/modules/files/files.service.ts
@@ -4,13 +4,15 @@ import { UploadFileRequest } from 'src/dto/file';
 import client from 'src/db/prismaClient';
 import { catchError, firstValueFrom } from 'rxjs';
 import { AxiosError } from 'axios';
+import * as officeParser from 'officeparser';
+import * as parseRTF from 'rtf-parser';
 
 @Injectable()
 export class FilesService {
   private readonly logger = new Logger(FilesService.name);
   constructor(private readonly httpService: HttpService) {}
 
-  async validateFile(content: Buffer): Promise<boolean> {
+  async validateFile(parsedText: string): Promise<boolean> {
     try {
       if (!process.env.VALIDATE_FILES) {
         return true;
@@ -20,7 +22,7 @@ export class FilesService {
         this.httpService
           .post(
             'http://127.0.0.1:8000/validate/file/',
-            { text: content.toString() },
+            { text: parsedText },
             {
               headers: {
                 'x-api-key': process.env.AI_API_KEY,
@@ -61,7 +63,88 @@ export class FilesService {
     file: Express.Multer.File,
   ): Promise<void> {
     try {
-      const isValid = await this.validateFile(file.buffer);
+      const fileType = file.originalname.split('.').pop().toLowerCase();
+
+      let parsedText;
+
+      if (fileType === 'pdf') {
+        try {
+          parsedText = (
+            await officeParser.parseOfficeAsync(file.buffer)
+          ).trim();
+        } catch (error) {
+          this.logger.error(`Failed to parse pdf file: ${error.message}`);
+          throw new HttpException(
+            'Failed to parse pdf file!',
+            HttpStatus.INTERNAL_SERVER_ERROR,
+          );
+        }
+      } else if (fileType === 'docx') {
+        try {
+          parsedText = await officeParser.parseOfficeAsync(file.buffer);
+        } catch (error) {
+          this.logger.error(`Failed to parse docx file: ${error.message}`);
+          throw new HttpException(
+            'Failed to parse docx file!',
+            HttpStatus.INTERNAL_SERVER_ERROR,
+          );
+        }
+      } else if (fileType === 'xlsx') {
+        try {
+          parsedText = await officeParser.parseOfficeAsync(file.buffer);
+        } catch (error) {
+          this.logger.error(`Failed to parse xlsx file: ${error.message}`);
+          throw new HttpException(
+            'Failed to parse xlsx file!',
+            HttpStatus.INTERNAL_SERVER_ERROR,
+          );
+        }
+      } else if (fileType === 'rtf') {
+        try {
+          parseRTF.string(file.buffer, function (err, doc) {
+            if (err) throw err;
+            parsedText = '';
+            doc.content.forEach(function (element) {
+              if (element.content && element.content.length > 0) {
+                element.content.forEach(function (innerElement) {
+                  if (innerElement.value) {
+                    parsedText += innerElement.value;
+                  }
+                });
+              }
+            });
+          });
+        } catch (error) {
+          this.logger.error(`Failed to parse rtf file: ${error.message}`);
+          throw new HttpException(
+            'Failed to parse rtf file!',
+            HttpStatus.INTERNAL_SERVER_ERROR,
+          );
+        }
+      } else if (fileType === 'txt') {
+        try {
+          const txtData = await file.buffer.toString('utf-8');
+          parsedText = txtData;
+        } catch (error) {
+          this.logger.error(`Failed to parse txt file: ${error.message}`);
+          throw new HttpException(
+            'Failed to parse txt file!',
+            HttpStatus.INTERNAL_SERVER_ERROR,
+          );
+        }
+      } else if (fileType === 'pptx') {
+        try {
+          parsedText = await officeParser.parseOfficeAsync(file.buffer);
+        } catch (error) {
+          this.logger.error(`Failed to parse pptx file: ${error.message}`);
+          throw new HttpException(
+            'Failed to parse pptx file!',
+            HttpStatus.INTERNAL_SERVER_ERROR,
+          );
+        }
+      }
+
+      const isValid = await this.validateFile(parsedText);
 
       if (!isValid) {
         this.logger.error('Validation failed. File not uploaded.');
@@ -75,7 +158,7 @@ export class FilesService {
       await client.file.create({
         data: {
           filename: uploadFileRequest.filename,
-          text_content: file.buffer.toString('utf-8'),
+          text_content: parsedText,
           content: file.buffer,
           size: file.size,
           uploaded_at: new Date(),
