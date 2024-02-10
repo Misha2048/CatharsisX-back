2d4d7449442fb34b9fe8d376d7067229da09c662
Spinkll <roma.skorohod.2005@gmail.com>
2024-02-11 00:28:01 +0200

    roma durak

--- src/dto/answer.ts
+++ src/dto/answer.ts
@@ -14,16 +14,3 @@ export class CreateAnswerResponseDto {
     this.message = message;
   }
 }
-
-export class HTTPError {
-  @ApiProperty()
-  error: string;
-
-  @ApiProperty()
-  statusCode: number;
-
-  constructor(error: string, statusCode: number = HttpStatus.BAD_REQUEST) {
-    this.error = error;
-    this.statusCode = statusCode;
-  }
-}
