a49cb364646dfcb2d0811e6fe8364ba70771d05d
Spinkll <95828180+Spinkll@users.noreply.github.com>
2024-02-11 20:37:31 +0200

    Add a file object to a create file response (#73)

--- src/dto/file.ts
+++ src/dto/file.ts
@@ -21,13 +21,25 @@ export class UploadFileErrorResponseDto {
   }
 }
 
-export class UploadFileSuccessResponseDto {
-  @ApiProperty()
-  message: string;
-
-  constructor(message: string) {
-    this.message = message;
+export class UploadFileResponseDto {
+  constructor(file: IFile) {
+    this.id = file.id;
+    this.name = file.filename;
+    this.size = file.size;
+    this.uploadedAt = file.uploaded_at;
   }
+
+  @ApiProperty({ description: 'The file ID' })
+  id: string;
+
+  @ApiProperty({ description: 'The file name' })
+  name: string;
+
+  @ApiProperty({ description: 'The size of a file in bytes', example: 1024 })
+  size: number;
+
+  @ApiProperty({ example: '2024-01-01T12:30:00.000' })
+  uploadedAt: Date;
 }
 
 export class GetFilesRequestDto {
