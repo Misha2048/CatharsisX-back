b67b19a2af916ce638ababd21d86dcabd94f0387
Spinkll <roma.skorohod.2005@gmail.com>
2024-02-20 10:27:05 +0200

    fix

--- src/dto/answer.ts
+++ src/dto/answer.ts
@@ -1,13 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Answer } from '@prisma/client';
 import { Transform, TransformFnParams } from 'class-transformer';
-import { IsNotEmpty, IsOptional, Length } from 'class-validator';
+import { IsNotEmpty, Length } from 'class-validator';
 
 export class CreateAnswerRequestDto {
   @ApiProperty()
   @IsNotEmpty({ message: 'Body should not be empty' })
-  @Length(1, 1000, {
-    message: 'Body length should be between 1 and 1000 characters',
+  @Length(1, 5000, {
+    message: 'Body length should be between 1 and 5000 characters',
   })
   @Transform(({ value }: TransformFnParams) => value?.trim())
   body: string;
@@ -25,8 +25,8 @@ export class CreateAnswerResponseDto {
 export class UpdateAnswerRequestDto {
   @ApiProperty()
   @IsNotEmpty({ message: 'Body should not be empty' })
-  @Length(1, 1000, {
-    message: 'Body length should be between 1 and 1000 characters',
+  @Length(1, 5000, {
+    message: 'Body length should be between 1 and 5000 characters',
   })
   @Transform(({ value }: TransformFnParams) => value?.trim())
   body: string;
